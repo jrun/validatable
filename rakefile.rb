2c43646d6f9538981e604167500d3fab874a62ea
jaycfields <jaycfields>
2007-03-25 22:20:32 +0000

    moving revision number

--- rakefile.rb
+++ rakefile.rb
@@ -29,7 +29,7 @@ Gem::manage_gems
 specification = Gem::Specification.new do |s|
 	s.name   = "validatable"
   s.summary = "Validatable is a library for adding validations."
-	s.version = "1.1.1"
+	s.version = "1.1.2"
 	s.author = 'Jay Fields'
 	s.description = "Validatable is a library for adding validations."
 	s.email = 'validatable-developer@rubyforge.org'
