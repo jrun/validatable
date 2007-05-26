4d0f248e3f0d56b303da3dcca7f1c77b86b6b294
jaycfields <jaycfields>
2007-05-26 23:12:39 +0000

    adding Dan as a contributor

--- lib/understandable.rb
+++ lib/understandable.rb
@@ -4,21 +4,21 @@ module Validatable
       def understands(*args)
         understandings.concat args
       end
-    
+
       def understandings
         @understandings ||= []
       end
-    
+
       def all_understandings
         return understandings + self.superclass.all_understandings if self.superclass.respond_to? :all_understandings
         understandings
       end
     end
-    
+
     def self.included(klass)
       klass.extend ClassMethods
     end
-    
+
     def must_understand(hash)
       invalid_options = hash.inject([]) do |errors, (key, value)|
         errors << key.to_s unless self.class.all_understandings.include?(key)
--- lib/validatable_assertions.rb
+++ lib/validatable_assertions.rb
@@ -1,18 +1,18 @@
 module ValidatableAssertions
-  
+
   def create_message_for_assertion(assertion)
     message = "#{assertion.klass} does not contain a #{assertion.validation_type} for #{assertion.attribute}"
     message += " with options #{assertion.options.inspect}" unless assertion.options == {}
     message
   end
-  
+
   def create_backtrace
     backtrace = caller
     backtrace.shift
     backtrace.shift
     backtrace
   end
-  
+
   def validation_matching_proc(assertion)
     lambda do |validation|
       result = assertion.validation_type === validation
@@ -25,13 +25,13 @@ module ValidatableAssertions
       result
     end
   end
-  
+
   def self.included(klass)
     Test::Unit::TestCase.class_eval do
       def self.create_test_name(assertion)
         "test#{assertion.validation_type.to_s.gsub(/Validatable::/,'').gsub(/([A-Z])/, '_\1').downcase}_#{assertion.attribute}"
       end
-      
+
       def self.define_test_method name, &block
         class_eval do
           define_method name, &block
