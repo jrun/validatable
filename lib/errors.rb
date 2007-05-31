e0f3226d27c4b606d7699d2cebf850d903c74404
jaycfields <jaycfields>
2007-05-31 01:41:21 +0000

    adding patch from Karthik Krishnan and Venkat for full errors

--- lib/errors.rb
+++ lib/errors.rb
@@ -2,9 +2,9 @@ module Validatable
   class Errors
     extend Forwardable
     include Enumerable
-    
+
     def_delegators :errors, :clear, :each, :each_pair, :empty?, :length, :size
-    
+
     # call-seq: on(attribute)
     # 
     # Returns nil, if no errors are associated with the specified attribute.
@@ -12,11 +12,11 @@ module Validatable
     def on(attribute)
       errors[attribute.to_sym]
     end
-    
+
     def add(attribute, message) #:nodoc:
       errors[attribute.to_sym] = message
     end
-    
+
     def merge!(errors) #:nodoc:
       errors.each_pair{|k, v| add(k,v)}
       self
@@ -24,6 +24,31 @@ module Validatable
 
     def errors #:nodoc:
       @errors ||= {}
+    end
+
+    def count
+      size
+    end
+
+    def full_messages
+      full_messages = []
+
+      errors.each_key do |attribute|
+        errors[attribute].each do |msg|
+          next if msg.nil?
+
+          if attribute.to_s == "base"
+            full_messages << msg
+          else
+            full_messages << humanize(attribute.to_s) + " " + msg
+          end
+        end
+      end
+      full_messages
+    end
+    
+    def humanize(lower_case_and_underscored_word)
+      lower_case_and_underscored_word.to_s.gsub(/_id$/, "").gsub(/_/, " ").capitalize
     end
   end
 end
--- test/unit/errors_test.rb
+++ test/unit/errors_test.rb
@@ -7,6 +7,23 @@ class ErrorsTest < Test::Unit::TestCase
     errors.on(:attribute)
   end
   
+  expect "Capitalized word" do
+    errors = Validatable::Errors.new
+    errors.humanize("capitalized_word")
+  end
+  
+  expect "Capitalized word without" do
+    errors = Validatable::Errors.new
+    errors.humanize("capitalized_word_without_id")
+  end
+  
+  expect ["A humanized message", "a base message"] do
+    errors = Validatable::Errors.new
+    errors.add(:base, "a base message")
+    errors.add(:a_humanized, "message")
+    errors.full_messages.sort
+  end
+  
   test "includes enumerable" do
     assert_equal true, Validatable::Errors.included_modules.include?(Enumerable)
   end
